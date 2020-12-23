89d6815586353a2503948b9627d2e110a4c87bab
BunnyBlack <xyy6836@126.com>
2020-12-23 20:12:11 +0800

    update

---
+++ DesignPatternsInCSharpAnddotNet/Journal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DesignPatternsInCSharpAnddotNet
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count;
+        }
+
+        public void RemoveEntry()
+        {
+            entries.RemoveAt(count - 1);
+            count--;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+}
--- DesignPatternsInCSharpAnddotNet/Program.cs
+++ DesignPatternsInCSharpAnddotNet/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace DesignPatternsInCSharpAnddotNet
+﻿using System;
+using System.Diagnostics;
+
+namespace DesignPatternsInCSharpAnddotNet
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            var journal = new Journal();
+            journal.AddEntry("I cried today");
+            journal.AddEntry("I ate a bug");
+            Console.WriteLine(journal);
+
+            var persistence = new Persistence();
+            const string filename = @"../../journal.txt";
+            persistence.SaveToFile(journal, filename, true);
         }
     }
 }
